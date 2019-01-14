b4b4df1dc8be2f38c1a3e5775c9eef8e7c093016
Maria <chau.marialuisa@gmail.com>
2019-01-14 10:56:04 -0500

    Add files for automation task #2

---
+++ MyFirstTask.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObjects;
+using System;
+
+namespace Task2
+{
+    public class MyFirstTask
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void myFirstTest()
+        {
+            
+            driver.Navigate().GoToUrl("https://stage1-vote.pollstream.com/8610");
+
+            // Message page
+            PollPage pollPage1 = new PollPage(driver);
+            pollPage1.ContinueButtonClick();
+
+            PollPage pollPage2 = new PollPage(driver);
+            // Q1
+            pollPage2.SelectSingleRadio();
+            //Q2
+            pollPage2.SelectRadioDropDown();
+            //Q3
+            pollPage2.SelectRadioImage(); 
+            pollPage2.SubmitButtonClick();
+
+            //Results page
+            PollPage pollPage3 = new PollPage(driver);
+            pollPage3.ResultContinueButtonClick();
+
+            // Q4 - Q5
+            PollPage pollPage4 = new PollPage(driver);
+            pollPage4.SubmitButtonClick();
+
+            //Results page
+            PollPage pollPage5 = new PollPage(driver);
+            pollPage5.ResultContinueButtonClick();
+
+            PollPage pollPage6 = new PollPage(driver);
+            // Q6
+            IWebElement RatingRadioQuestion = pollPage6.GetRatingRadioQuestion();
+            pollPage6.SelectRatingRadio(RatingRadioQuestion);
+            // Q7
+            IWebElement RatingRadioNAQuestion = pollPage6.GetRatingRadioWithNaQuestion();
+            pollPage6.SelectRatingRadioNa(RatingRadioNAQuestion);
+            // Q8
+            IWebElement RatingDropdownQuestion = pollPage6.GetRatingDropdownQuestion();
+            pollPage6.SelectRatingDropdown(RatingDropdownQuestion);
+            // Q9
+            IWebElement RatingDropdownNaQuestion = pollPage6.GetRatingDropdownNaQuestion();
+            pollPage6.SelectRatingDropdownNa(RatingDropdownNaQuestion);
+            pollPage6.SubmitButtonClick();
+
+            // Results page
+            PollPage pollPage7 = new PollPage(driver);
+            pollPage7.ResultContinueButtonClick();
+
+            PollPage pollPage8 = new PollPage(driver);
+            // Q10
+            pollPage8.ContinueButtonClick();
+
+            // Results page
+            PollPage pollPage9 = new PollPage(driver);
+            pollPage9.ResultContinueButtonClick();
+
+            PollPage pollPage10 = new PollPage(driver);
+            // Q11
+            pollPage10.ContinueButtonClick();
+
+            //Results page
+            PollPage pollPage11 = new PollPage(driver);
+            pollPage11.ResultContinueButtonClick();
+
+            PollPage pollPage12 = new PollPage(driver);
+            var acknowledgementMessage = driver.FindElement(By.ClassName("ps-acknowledgement"));
+            Assert.That(acknowledgementMessage.Displayed, "Acknowledgement message is not being displayed");
+            Assert.AreEqual("a really short message -thanks", acknowledgementMessage.Text);
+        }
+    }
+}
---
+++ PollPage.cs
@@ -0,0 +1,136 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+
+namespace PageObjects
+{
+    class PollPage
+    {
+        private const string Continue = "ps-input-submit";
+        private const string Submit = "ps-vote-button";
+        private const string Result = "result-continue-button";
+
+        private IWebDriver driver;
+
+        public PollPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        // safer to find element by Id, maybe multiple instances in HTML with this classname
+        private IWebElement SubmitButton { get { return driver.FindElement(By.ClassName(Submit)); } }
+        private IWebElement ContinueButton { get { return driver.FindElement(By.ClassName(Continue)); } }
+        private IWebElement ResultContinueButton { get { return driver.FindElement(By.ClassName(Result)); } }
+     
+        // for the question types, should probably pass in a variable instead of hard-coding the element so all polls/surveys/forms using these question types can use these 
+        private IWebElement GetSingleRadioQuestion()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            return driver.FindElement(By.Id("question-79364"));
+        }
+
+        public IWebElement GetRadioDropDownQuestion() { return driver.FindElement(By.Id("answer_id")); }
+        public IWebElement GetSingleRadioImageQuestion() { return driver.FindElement(By.Id("ps_answer_PSPoll_268831")); }
+        public IWebElement GetRatingRadioQuestion() { return driver.FindElement(By.Id("question-79368")); }
+        public IWebElement GetRatingRadioWithNaQuestion() { return driver.FindElement(By.Id("question-79371")); }
+        public IWebElement GetRatingDropdownQuestion() { return driver.FindElement(By.Id("question-79370")); }
+        public IWebElement GetRatingDropdownNaQuestion() { return driver.FindElement(By.Id("question-79369")); }
+
+        public IWebElement GetDropdownQuestion(string id) { return driver.FindElement(By.Id(id));  } 
+
+        public void SelectSingleRadio()
+        {
+            GetSingleRadioQuestion().FindElement(By.Id("ps_answer_PSPoll_268840")).Click();
+        }
+
+        public void SelectRadioDropDown()
+        {
+            {
+                SelectDropdown(GetRadioDropDownQuestion(), "option[value=':268838|1'");
+            }
+        }
+
+        public void SelectRadioImage()
+        {
+            {
+                Actions action = new Actions(driver);
+                action.MoveToElement(driver.FindElement(By.Id("ps_answer_PSPoll_268831"))).Click().Perform();
+            }
+        }
+
+        // Radio questions are the same, whether or not NA is included.  I separated them just in case there was a scenario where N/A had to be chosen.
+        // Could possibly pass in a boolean in the method to show whether n/a is included or not and adjust method accordingly
+        public void SelectRatingRadio(IWebElement radioQuestion)
+        {
+            SelectRating(GetRatingRadioQuestion(), "ps_answer_PSPoll_268852-3");
+            SelectRating(GetRatingRadioQuestion(), "ps_answer_PSPoll_268853-6");
+            SelectRating(GetRatingRadioQuestion(), "ps_answer_PSPoll_268854-10");
+            SelectRating(GetRatingRadioQuestion(), "ps_answer_PSPoll_268855-1");
+        }
+        
+        public void SelectRatingRadioNa(IWebElement radioNaQuestion)
+        {
+            SelectRating(GetRatingRadioWithNaQuestion(), "ps_answer_PSPoll_268864-na");
+            SelectRating(GetRatingRadioWithNaQuestion(), "ps_answer_PSPoll_268865-na");
+            SelectRating(GetRatingRadioWithNaQuestion(), "ps_answer_PSPoll_268866-na");
+            SelectRating(GetRatingRadioWithNaQuestion(), "ps_answer_PSPoll_268867-na");
+        }
+
+        // Rating dropdown questions are the same,  whether or not NA is included.  I separated them just in case there was a scenario where N/A had to be chosen.
+        // Could possibly pass in a boolean in the method to show whether n/a is included or not and adjust method accordingly
+        public void SelectRatingDropdown (IWebElement ratingQuestion)
+        {
+            SelectDropdown(GetRatingDropdownQuestion(), "option[value=':268860|1:1'");
+            SelectDropdown(GetRatingDropdownQuestion(), "option[value=':268861|1:2'");
+            SelectDropdown(GetRatingDropdownQuestion(), "option[value=':268862|1:3'");
+            SelectDropdown(GetRatingDropdownQuestion(), "option[value=':268863|1:4'");
+        }
+
+        public void SelectRatingDropdownNa (IWebElement ratingNaQuestion)
+        {
+            SelectDropdown(GetRatingDropdownNaQuestion(), "option[value=':268856|1:1'");
+            SelectDropdown(GetRatingDropdownNaQuestion(), "option[value=':268857|1:2'");
+            SelectDropdown(GetRatingDropdownNaQuestion(), "option[value=':268858|1:3'");
+            SelectDropdown(GetRatingDropdownNaQuestion(), "option[value=':268859|1:5'");
+        }
+
+        public void SubmitButtonClick()
+        {
+            SubmitButton.Click();
+        }
+
+        public void ContinueButtonClick()
+        {
+            ContinueButton.Click();
+        }
+
+        public void ResultContinueButtonClick()
+        {
+            bool staleElement = true;
+            while (staleElement)
+            {
+                try
+                {
+                    ResultContinueButton.Click();
+                    staleElement = false;
+
+                }
+                catch (StaleElementReferenceException e)
+                {
+                    staleElement = true;
+                }
+            }
+        }
+
+        private void SelectRating(IWebElement question, string rating)
+        {
+            question.FindElement(By.Id(rating)).Click();
+        }
+
+        private void SelectDropdown(IWebElement question, string option)
+        {
+            question.FindElement(By.CssSelector(option)).Click();
+        }
+       
+    }
+}
